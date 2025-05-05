51a487d9d8b014cfd1b1b2f0bb61a257d935651a
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-05 22:27:41 +0530

    Google keep frontend

--- Frontend/src/Header.jsx
+++ Frontend/src/Header.jsx
@@ -1,24 +1,40 @@
 import React from 'react'
 import GoogleKeep from './assets/GoogleKeep.png'
 import { IoMenuOutline } from "react-icons/io5";
+import reactUserIcon from './assets/react.svg'
+import { CgMenuGridO } from "react-icons/cg";
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdRefresh } from "react-icons/md";
 function Header() {
   return (
     <div>
-        <div className='ring-1 ring-gray-500 flex px-4 py-1 items-center gap-0'>
+        <div className='w-full grid grid-cols-3 justify-evenly ring-1 border-gray-400 py-2 px-10'>
             <div className='w-[250px] flex gap-2 items-center'>
-                <div className='cursor-pointer px-2 py-2 hover:bg-slate-200 hover:rounded-full'>
-                    <span className='text-3xl '><IoMenuOutline /></span>
-                </div>
-                <div className='px-2 flex items-center'>
-                    <img src={GoogleKeep} alt="" />
-                    <p className='text-2xl font-bold'>Keep</p>
-                </div>
+                <div className='cursor-pointer px-2 py-2 hover:bg-slate-200 hover:rounded-full'>
+                    <span className='text-3xl '><IoMenuOutline /></span>
+                </div>
+                <div className='px-2 flex items-center'>
+                    <img src={GoogleKeep} alt="" />
+                    <p className='text-2xl font-bold'>Keep</p>
+                </div>
+            </div>
+            <div className=''>
+                <input className='w-full py-3 px-6 rounded-md bg-slate-200 outline-0' type="text"placeholder='Search Note:- ' />
             </div>
-            <div>
-                <input className='px-4 py-3 rounded-md w-[800px] bg-slate-200' type="text" placeholder='Search'/>
-            </div>
-            <div>
 
+            <div className='flex justify-end pr-10 items-center gap-6'>
+                <div>
+                    <span className='inline-block text-2xl text-gray-500 cursor-pointer px-2 py-2 hover:bg-slate-200 hover:rounded-full'><MdRefresh /></span>
+                </div>
+                <div>
+                    <span className='inline-block text-2xl text-gray-500 cursor-pointer px-2 py-2 hover:bg-slate-200 hover:rounded-full'><IoSettingsOutline /></span>
+                </div>
+                <div>
+                    <span className='inline-block text-2xl text-gray-500 cursor-pointer px-2 py-2 hover:bg-slate-200 hover:rounded-full'><CgMenuGridO /></span>
+                </div>
+                <div className='w-12 h-12 rounded-full shadow-inner shadow-gray-500 flex justify-center items-center'>
+                    <img src={reactUserIcon} alt="" />
+                </div>
             </div>
         </div>
     </div>
