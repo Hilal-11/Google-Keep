b6a8555aa8ec7590591c95aa87ab0822e0246ff9
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-07 23:58:33 +0530

    done today

--- Frontend/src/config/StaticData.jsx
+++ Frontend/src/config/StaticData.jsx
@@ -20,3 +20,25 @@ export const paintColors = [
     {id: 7 , color: '#c4b5fd'},
 
 ];
+
+export const y = [
+    {id: 1 , color: '#14b8a6'},
+    {id: 2 , color: '#d8f999'},
+    {id: 3 , color: '#1d293d'},
+    {id: 4 , color: '#3b82f6'},
+    {id: 5 , color: '#fee685'},
+    {id: 6 , color: '#fdba74'},
+    {id: 7 , color: '#c4b5fd'},
+
+];
+
+export const x = [
+    {id: 1 , color: '#14b8a6'},
+    {id: 2 , color: '#d8f999'},
+    {id: 3 , color: '#1d293d'},
+    {id: 4 , color: '#3b82f6'},
+    {id: 5 , color: '#fee685'},
+    {id: 6 , color: '#fdba74'},
+    {id: 7 , color: '#c4b5fd'},
+
+];
