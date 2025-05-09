fbfa417453716e221c191fc019faa0b15ca1f961
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-09 15:07:41 +0530

    Frontend Routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import Header from './Components/Header'
 import MainBodyNote from './Components/MainBodyNote'
 import { ToastContainer } from 'react-toastify'
+import { Routes , Route } from 'react-router-dom'
+
+import Reminders from './Pages/Reminders'
+import Archive from './Pages/Archive'
+import Bin from './Pages/Bin'
+
 function App() {
   return (
     <div>
@@ -10,6 +16,14 @@ function App() {
         <Header />
         <MainBodyNote />
       </div>
+
+    <Routes>
+      <Route path='/reminder' element={<Reminders />} />
+      <Route path='/archive' element={<Archive />} />
+      <Route path='/bin' element={<Bin />} />
+    </Routes>
+
+
     </div>
   )
 }
