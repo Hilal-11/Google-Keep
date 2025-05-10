d9bff65dce1775e212f91b93385d9c1f4ab92684
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-10 14:52:45 +0530

    UI and authentication and autherization

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -40,13 +40,16 @@ function Header() {
                     <span className='hidden lg:inline-block text-2xl text-gray-500 cursor-pointer px-[6px] lg:px-2 lg:py-2 hover:bg-slate-200 hover:rounded-full'><IoSettingsOutline /></span>
             {
               isLoggedIn ? (                    
-                <Stack className='group relative' direction="row" spacing={2}>
+                <div className='relative group'>
+                  <Stack direction="row" spacing={2}>
                           <Avatar className='bg-black cursor-pointer  py-0 px-0' alt="Remy Sharp" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2PRnxPr8rX7NOWGRK63y4NGffjANp_V0qXYQ9msj1w0sqj7Zn8PhrI8jwpsmVZn8Lsc&usqp=CAU' />
-                          <div className='w-full bg-white z-50 group-hover:flex-col text-[14px] absolute right-0 lg:-left-10 top-14 py-2 rounded-sm poppins-regular text-center shadow-sm shadow-gray-400'>
+                          <div className='hidden group-hover:block w-[120px] bg-white z-50 text-[14px] absolute right-0 lg:-left-10 top-14 lg:top-10 py-2 rounded-sm poppins-regular text-center shadow-sm shadow-gray-400'>
                               <button className='px-2 py-1 whitespace-nowrap cursor-pointer hover:bg-slate-100 w-full'>Logout</button>
                               <button className='px-2 py-1 whitespace-nowrap cursor-pointer hover:bg-slate-100 w-full'>Varify Account</button>
                           </div>
-                </Stack>): (
+                  </Stack>
+                </div>  
+              ): (
                   <motion.button
                   initial={{
                     scale: 0.9
