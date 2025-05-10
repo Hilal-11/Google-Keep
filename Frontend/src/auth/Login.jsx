beace71f8fc740ac70c6f8e4c8c2c8ef03de387a
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-10 23:16:45 +0530

    done

--- Frontend/src/auth/Login.jsx
+++ Frontend/src/auth/Login.jsx
@@ -3,16 +3,17 @@ import React from 'react'
 function Login() {
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-        <div className='grid grid-cols-3 bg-slate-100 shadow-sm shadow-gray-400 rounded-lg h-[700px] w-[80%]'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 bg-slate-100 shadow-sm shadow-gray-400 rounded-lg h-[700px] w-[80%]'>
           {/* FORM CONTAINER */}
-          <div className='w-full bg-slate-300 rounded-tl-lg rounded-l-lg grid-rows-2'>
+          <div className=' w-full rounded-tl-lg rounded-l-lg col-span-2'>
 
           </div>
 
 
           {/* SHOWCASE CONTAINER */}
-          <div className='w-full bg-slate-950 rounded-tr-lg rounded-r-lg'></div>
-
+          <div className='w-full rounded-tr-lg rounded-r-lg bg-[url(https://jsmastery.pro/_next/image?url=%2Fassets%2Fgeneral%2Fimages%2Fhero-img.webp&w=750&q=75)] lg:w-full lg:h-[100%] bg-contain bg-center bg-no-repeat pr-20'>
+            
+          </div>
 
         </div>
     </div>
