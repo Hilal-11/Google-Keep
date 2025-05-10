27d1e1870b6d9cc5e74f8565c2744a2d440ffd20
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-10 22:42:04 +0530

    fronend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Components/Header'
 import MainBodyNote from './Components/MainBodyNote'
 import { ToastContainer } from 'react-toastify'
-import { Routes , Route } from 'react-router-dom'
+import { Routes , Route , useLocation } from 'react-router-dom'
 
 import Reminders from './Pages/Reminders'
 import Labels from './Pages/Labels'
@@ -11,14 +11,21 @@ import Bin from './Pages/Bin'
 import SideBar from './Components/SideBar'
 import { useContext } from 'react'
 import { AppContext } from './Context/ContextApi'
+import Login from './auth/Login'
 function App() {
 
   const { isOpenMenu } = useContext(AppContext)
+  const location = useLocation()
+  const noHeaderSidebarRoutes = ['/login']
+  const shouldShowHeaderSidebar = !noHeaderSidebarRoutes.includes(location.pathname)
+
   return (
     <div>
-      <div className=''>
-        <ToastContainer />
-        <Header />
+      {
+        shouldShowHeaderSidebar && (
+        <div className=''>
+          <ToastContainer />
+          <Header />
         {
           isOpenMenu &&
           <div className='block mb-10'>
@@ -27,16 +34,22 @@ function App() {
         }
         <div className='hidden lg:block mb-10'>
           <SideBar />
-        </div> 
-      </div>
-    <Routes>
-      <Route path='/' element={<MainBodyNote />}/>
-      <Route path='/reminder' element={<Reminders />} />
-      <Route path='/labels' element={<Labels />} />
-      <Route path='/archive' element={<Archive />} />
-      <Route path='/bin' element={<Bin />} />
-    </Routes>
+          </div> 
+        </div>
+        )
+      }
+        <Routes>
+          <Route path='/' element={<MainBodyNote />}/>
+          <Route path='/reminder' element={<Reminders />} />
+          <Route path='/labels' element={<Labels />} />
+          <Route path='/archive' element={<Archive />} />
+          <Route path='/bin' element={<Bin />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
     </div>
+
+          
+
   )
 }
 
--- Frontend/src/auth/Login.jsx
+++ Frontend/src/auth/Login.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='w-full h-screen flex justify-center items-center'>
+        <div className='flex bg-slate-100 shadow-sm shadow-gray-400 rounded-lg h-[760px] w-2/3'>
+          {/* FORM CONTAINER */}
+          <div></div>
+          {/* SHOWCASE CONTAINER */}
+          <div></div>
+        </div>
+    </div>
   )
 }
 
