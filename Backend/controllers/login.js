9ab55c02b41a42a9b098223dc46e4ffc424ef1ab
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-11 22:38:43 +0530

    Login handler is complete

--- Backend/controllers/login.js
+++ Backend/controllers/login.js
@@ -1,9 +1,41 @@
-
+const Model = require('../models/authSchema')
+const bcrypt = require('bcrypt')
 const login = async (req , res) => {
+    const { email , password } = req.body;
+    if(!email || !password) {
+        return res.json({
+            success: true,
+            message: "form details missing"
+        })
+    }
     try {
+        // CHECK IF USER EXISTS
+        const userExists = await Model.findOne({ email })
+        if(!userExists) {
+            return res.json( {
+                success: false,
+                message: "incorrect email and password"
+            })
+        }
 
+        //  IF USER EXISTS THEN 
+        const isMatch = bcrypt.compare(userExists.password);
+        if(!isMatch) {
+            return res.json({
+                success: false,
+                message: "incorrect password"
+            })
+        }
+                                        // GENERATE JWT( JSON WEB TOKEN )
+        res.json({
+            success: true,
+            message: "Login successfully"
+        })
     }catch(error) {
-
+        return res.json({
+            success: false,
+            message: "Failed to login, incorrect emial and password"
+        })
     }
 }
 
