4efb193872196784cfc1c8516c716abda9b81ecf
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-12 02:44:48 +0530

    done

--- Backend/routes/app_routes.js
+++ Backend/routes/app_routes.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const signUp = require('../controllers/signUp')
 const login = require('../controllers/login')
 const getUser = require('../controllers/getUser')
+const keepNote = require('../controllers/keepNote')
 
 router.post("/signUp" , signUp);
 router.post("/login" , login);
 router.get("/getUserInfo" , getUser);
+router.post("/keepNote" , keepNote)
 
 module.exports = router
