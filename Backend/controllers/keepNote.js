a0c347f361f9e0913b5765781489b4c36477775f
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-12 02:55:52 +0530

    keep Note controller

---
+++ Backend/controllers/keepNote.js
@@ -0,0 +1,31 @@
+const Note = require('../models/NoteSchema');
+
+const keepNote = async (req , res) => {
+    const { title , content , color } = req.body;
+    if(!title || !content || !color) {
+        return res.json({
+            success: false,
+            message: "Missing note requirements"
+        })
+    }
+    try{
+        const response = await Note.create({
+            title,
+            content,
+            color,
+        })
+
+        return res.json({
+            success: true,
+            message: "Successfully create a keep note on DB"
+        })
+
+    }catch(error) {
+        return res.json({
+            success: false,
+            message: "Failed to create Note"
+        })
+    }
+}
+
+module.exports = keepNote
