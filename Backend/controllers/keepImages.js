7dc1f5b3c9fedc84c6336030d7b87dd37fd79a5b
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-17 23:25:03 +0530

    keep images

---
+++ Backend/controllers/keepImages.js
@@ -0,0 +1,12 @@
+
+const keepImages = async(req , res) => {
+    try{
+        const { } = req.body;
+
+    }catch(error) {
+        return res.json({
+            success: false,
+            message: "failed to keep images with notes"
+        }) 
+    }
+}
