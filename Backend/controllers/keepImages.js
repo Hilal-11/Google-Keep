d028a0ce70fd111a86bda7708c94eb98bec81774
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-17 23:40:27 +0530

    done

--- Backend/controllers/keepImages.js
+++ Backend/controllers/keepImages.js
@@ -10,7 +10,13 @@ const keepImages = async(req , res) => {
         const imageType = `${imageFile.name.split('.')[1].toLowerCase()}` // extract the image type or extension
         const supportImages = ['jpg', 'png', 'jpeg' , 'webp']; // images upload support extensions
         const isSupport = checkImageUploadSuport(imageType ,supportImages)   // checking for extension support
-
+        
+        if(!isSupport) {
+            return res.json({
+                success: false,
+                message: "Image type not support"
+            })
+        }
     }catch(error) {
         return res.json({
             success: false,
