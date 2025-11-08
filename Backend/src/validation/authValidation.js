cc89c53e31603ff169affad5313db53e43dc117c
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-08 23:27:24 +0530

    ...

--- Backend/src/validation/authValidation.js
+++ Backend/src/validation/authValidation.js
@@ -2,24 +2,60 @@ import { body , params } from "express-validator"
 
 const signUpValidator = () => {
     return [
-
+        body("username")
+            .isString()
+            .notEmpty().withMessage("Username is required")
+            .trim()
+        ,
+        body("email")
+            .isString()
+            .notEmpty().withMessage("Email is required")
+            .isEmail().withMessage("Email is invalid")
+            .trim()
+        ,
+        body("password")
+            .isString()
+            .trim()
+            .notEmpty().withMessage("Password is required")
+            .isLength({ min: 8 }).withMessage("Password should contain atleast 8 characters")
+            .isLength({ max: 12}).withMessage("Password should only contain 12 characters")
+        ,
+        body("role")
+            .optional()
+            .isIn(AvailableUserRoles)
+            .withMessage("Invalid user role"),
     ]
 }
 
 const loginValidator = () => {
     return [
-
+        body("email")
+            .isString()
+            .notEmpty().withMessage("Email is required")
+            .isEmail().withMessage("Email is invalid")
+            .trim()
+            
+        ,
+        body("password")
+            .isString()
+            .trim()
+            .notEmpty().withMessage("Password is required")
+            .isLength({ min: 8 }).withMessage("Password should contain atleast 8 characters")
+            .isLength({ max: 12}).withMessage("Password should only contain 12 characters")
+        ,
     ]
 }
 const resetPasswordValidator = () => {
     return [
-
+        body("oldPassword").notEmpty().withMessage("Old password is required"),
+        body("newPassword").notEmpty().withMessage("New password is required"),
     ]
 }
 
 const changeCurrentPasswordValidator = () => {
     return [
-        
+        body("oldPassword").notEmpty().withMessage("Old password is required"),
+        body("newPassword").notEmpty().withMessage("New password is required"),
     ]
 }
 
--- Backend/src/validation/keepValidation.js
+++ Backend/src/validation/keepValidation.js
@@ -6,6 +6,8 @@ const createNoteValidator = () =>  {
             .isString()
             .notEmpty()
             .withMessage("Note title is required")
+            .isLength({ max: 20 }).withMessage("Title should be contains atmost 20 characters")
+
             
         ,
         body("note_discription")
@@ -18,7 +20,20 @@ const createNoteValidator = () =>  {
 
 const updateNoteValidator = () => {
     return [
+        body("note_title")
+            .isString()
+            .notEmpty()
+            .trim()
+            .withMessage("Note title is required")
+            .isLength({ max: 20 }).withMessage("Title should be contains atmost 20 characters")
 
+        ,
+        body("note_discription")
+            .isString()
+            .notEmpty()
+            .trim()
+            .withMessage("Discription is required")
+        ,
     ]
 }
 
