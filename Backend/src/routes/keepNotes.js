6d5a0155a1706ee8b84998ac44b6d48d2190f61c
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-08 23:02:21 +0530

    Improve keepNotes routing with cleaner structure and middleware support

--- Backend/src/routes/keepNotes.js
+++ Backend/src/routes/keepNotes.js
@@ -1,15 +1,4 @@
 import express from ('express');
-const keepNote_routes = express.Router()
+const router = express.Router()
 
-import getNotes from ('../controllers/getNotes')
-import keepNote from ('../controllers/keepNote')
-import deleteNote from ('../controllers/deleteNote')
-import updateNote from ('../controllers/updateNote')
-
-keepNote_routes.get("/getNotes" , getNotes)
-keepNote_routes.post("/keepNote" , keepNote)
-keepNote_routes.post("/deleteNote:id", deleteNote)
-keepNote_routes.post("/updateNote:id", updateNote)
-
-
-module.exports = keepNote_routes
+module.exports = router
