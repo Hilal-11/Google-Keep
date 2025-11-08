496ddcc18db8de5833f266c76741f55eec3d6316
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-08 23:02:21 +0530

    Refactor authentication routes and integrate new validation middleware

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,12 +1,5 @@
 import express from'express';
 const router = express.Router()
 
-import signUp from '../controllers/signUp'
-import login from '../controllers/login'
-import getUser from '../controllers/getUser'
-
-router.post("/signUp" , signUp);
-router.post("/login" , login);
-router.get("/getUserInfo" , getUser);
 
 module.exports = router
