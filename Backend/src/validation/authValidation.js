88d55200b0e4457b684029066a17b38b3c8f77a6
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-08 23:02:22 +0530

    Add dedicated validation schema for authentication endpoints

---
+++ Backend/src/validation/authValidation.js
@@ -0,0 +1,26 @@
+import { body , params } from "express-validator"
+
+const signUpValidator = () => {
+    return [
+
+    ]
+}
+
+const loginValidator = () => {
+    return [
+
+    ]
+}
+const resetPasswordValidator = () => {
+    return [
+
+    ]
+}
+
+const changeCurrentPasswordValidator = () => {
+    return [
+        
+    ]
+}
+
+export default { signUpValidator , loginValidator , resetPasswordValidator , changeCurrentPasswordValidator }
