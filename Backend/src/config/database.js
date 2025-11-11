2b2ad2465aaab22fe4dfeee431fbef84dba5e79c
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-11 18:48:55 +0530

    Improve database connection logic and error handling

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,31 +1,44 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import logger from "../logger/winston.logger"
+import logger from "../logger/winston.logger.js"
 dotenv.config();
 
+// const connectDB = async () => {
+//     try{
+//         await mongoose.connect(process.env.DATABASE_URL);
+//         logger.info("Database connection successfully")
+//     }catch(error){
+//         logger.error("Database connection failed", { error: error.message })
+//         process.exit(1);
+//     }
+//     mongoose.connection.on("connected" , () => {
+//         logger.info("MongoDB connected")
+//     })
+//     mongoose.connection.on("error", (error) => {
+//         logger.error("MongoDB connection error", { error: error.message })
+//     })
+//     mongoose.connection.on('disconnected', () => {
+//         logger.warn('MongoDB disconnected');
+//     });
+
+//     process.on("SIGINT" , async () => {
+//         await mongoose.connection.close();
+//         logger.info('MongoDB connection closed due to app termination');
+//         process.exit(0);
+//     })
+
+// }
+
+
 const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.DATABASE_URL);
-        logger.info("Database connection successfully")
-    }catch(error){
-        logger.error("Database connection failed", { error: error.message })
+    try {
+        const conn = await mongoose.connect(process.env.DATABASE_URL);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error("Database connection failed", error);
         process.exit(1);
     }
-    mongoose.connection.on("connected" , () => {
-        logger.info("MongoDB connected")
-    })
-    mongoose.connection.on("error", (error) => {
-        logger.error("MongoDB connection error", { error: error.message })
-    })
-    mongoose.connection.on('disconnected', () => {
-        logger.warn('MongoDB disconnected');
-    });
 
-    process.on("SIGINT" , async () => {
-        await mongoose.connection.close();
-        logger.info('MongoDB connection closed due to app termination');
-        process.exit(0);
-    })
-
+    
 }
 export default connectDB
