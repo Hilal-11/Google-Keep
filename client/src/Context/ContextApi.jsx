e5a052488a9f87e223c499a3263d36c8886590f5
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-28 23:34:29 +0530

    Update context logic and state management

---
+++ client/src/Context/ContextApi.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState , useEffect, createContext } from "react";
+
+
+export const AppContext = createContext();
+export const AppContextProvider = ({ children }) => {
+
+    const [isOpenMenu , setIsOpenMenu] = useState(false);
+    const [isLoggedIn , setIsLoggedIn] = useState(false)
+
+    const storeTokenInLocalStorage = (token) => {
+        return localStorage.setItem('token' , token);
+    }
+    const states = {
+        isOpenMenu, setIsOpenMenu,
+        isLoggedIn, setIsLoggedIn,
+        storeTokenInLocalStorage
+    }
+    return(
+        <AppContext.Provider value={states}>
+            { children }
+        </AppContext.Provider>
+    )
+} 
+
