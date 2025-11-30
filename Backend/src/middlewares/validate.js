bcf0cc5f37a0ba15475f9329da7c6835b34e0e8c
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-30 23:25:02 +0530

    backend

--- Backend/src/middlewares/validate.js
+++ Backend/src/middlewares/validate.js
@@ -3,7 +3,7 @@ import ApiError from "../utils/api-error.js";
 const validator = (req, res, next) => {
     const errors = validationResult(req);
     if(errors.isEmpty()) {
-        next();
+        return next();
     }
     const extractedErrors = [];
     errors.array().map((err) => extractedErrors.push({
