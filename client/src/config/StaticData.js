624fbc358fe437e33223870bc30527f317e4a96b
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-12-10 21:35:57 +0530

    Update static data configuration with new values

---
+++ client/src/config/StaticData.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.paintColors = exports.clipItems = void 0;
+exports.clipItems = [
+    { id: 1, clipItem: 'Delete note', type: "delete" },
+    { id: 2, clipItem: 'Add Label', type: "label" },
+    { id: 3, clipItem: 'Add drawing', type: "drawing" },
+    { id: 4, clipItem: 'Make a copy', type: "copy" },
+    { id: 5, clipItem: 'Show stick boxes', type: "stickBox" },
+    { id: 6, clipItem: 'Copy to Google Docs', type: "googleDocs" },
+    { id: 7, clipItem: 'Version history', type: "history" },
+];
+exports.paintColors = [
+    { id: 1, color: '#14b8a6' },
+    { id: 2, color: '#d8f999' },
+    { id: 3, color: '#1d293d' },
+    { id: 4, color: '#3b82f6' },
+    { id: 5, color: '#fee685' },
+    { id: 6, color: '#fdba74' },
+    { id: 7, color: '#c4b5fd' },
+];
