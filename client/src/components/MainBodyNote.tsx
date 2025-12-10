778eb9e9e616dc760948bf349fb9b281c7f92fa7
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-12-10 21:35:56 +0530

    Refactor MainBodyNote for cleaner structure and better UX

---
+++ client/src/components/MainBodyNote.tsx
@@ -0,0 +1,27 @@
+import { useContext } from 'react'
+import SideBar from './SideBar'
+import NoteContainer from './NoteContainer'
+import { AppContext } from '../Context/ContextApi'
+
+function MainBodyNote() {
+  const { isOpenMenu } = useContext(AppContext)
+  return (
+    <div className='w-[100%]'>
+    <div className='grid grid-cols-18 gap-1'>
+        {
+          isOpenMenu && <div className='block mb-10'>
+                          <SideBar />
+                        </div> 
+        }
+        <div className='hidden lg:block mb-10'>
+          <SideBar />
+        </div> 
+        <div className='absolute left-0 w-screen  mx-auto lg:col-span-16 my-16'>
+            <NoteContainer />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MainBodyNote
